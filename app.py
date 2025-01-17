0ca47a40dead1277c8229f8062a62bb47907dd5d
ahmed mustafa <ahmedmousa9171@gmailcom>
2025-01-17 20:45:38 +0300

    phase 2

---
+++ app.py
@@ -0,0 +1,6 @@
+# function to print current version 
+import sys 
+def print_version_and_exit(version):
+    print (f"Current app version :{version}")
+    sys.exit()
+print_version_and_exit("3.12.1")
